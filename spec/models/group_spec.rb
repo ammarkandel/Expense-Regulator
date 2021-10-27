4d3d5b855004585d00b3764867241f5176d50f76
ammarkandel <merom8703@gmail.com>
2021-10-27 14:14:50 +0200

    Add testing for group

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
+  subject {
+    Group.new(name: 'group name', icon: 'default-group', user_id: 1)
+  }
+
+  it { should belong_to(:user).without_validating_presence }
+
+  it 'Expected group to be valid if add all attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'Expected group to be invalid if missing name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Expected group to be invalid if missing icon' do
+    subject.icon = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Expected group to be invalid missing user' do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
 end
