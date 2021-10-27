314c4e39c8a9251b2739687f3fa5e38b24667062
ammarkandel <merom8703@gmail.com>
2021-10-27 14:23:44 +0200

    Add testing for expenses

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
+  subject {
+    Expense.new(name: 'expense name', amount: 40, user_id: 1)
+  }
+
+  it { should belong_to(:user) }
+
+  it 'Expected expense to be valid if add all attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'Expected expense to be invalid if missing name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Expected expense to be invalid missing user' do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Expected expense to be invalid if missing amount' do
+    subject.amount = nil
+    expect(subject).to_not be_valid
+  end
 end
